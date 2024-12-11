a7ab27dff14892bf54890241e6625d61e39104a5
Myriam <ikdgk@plaiaundi.net>
2024-12-11 10:31:58 +0100

    commit myriam

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Kaixo, Rama nueva!");
         System.out.println("Nueva Rama Myriam");
+        System.out.println("Nuevo commit Myriam");
     }
 }
