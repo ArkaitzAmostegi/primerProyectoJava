917803f72771534452979b27a9bbb33217015786
Myriam <ikdgk@plaiaundi.net>
2024-12-11 10:43:38 +0100

    conflicto

--- src/App.java
+++ src/App.java
@@ -1,11 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Kaixo, Rama nueva!");
-        System.out.println("Nueva Rama Myriam");
         System.out.println("Nuevo commit Myriam");
         for (int i=0;i<10;i++){
             System.out.println("bucle por 10");
         }
         System.out.println("Kaixo, nuevo commit M");
+        System.out.println("Conflito!");
+
     }
 }
