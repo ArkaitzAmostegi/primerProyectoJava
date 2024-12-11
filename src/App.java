d6567db37661ca9676b5ced05cbfb49b9d6c0c22
Myriam <ikdgk@plaiaundi.net>
2024-12-11 10:23:52 +0100

    Añadido comentario Myriam

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Kaixo, Rama nueva!");
+        System.out.println("Nueva Rama Myriam");
     }
 }
