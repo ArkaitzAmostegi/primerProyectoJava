a9da57ba85218198fb422e3ec447526865613c6a
Myriam <ikdgk@plaiaundi.net>
2024-12-11 10:35:09 +0100

    commit borro sysout

--- src/App.java
+++ src/App.java
@@ -2,6 +2,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Kaixo, Rama nueva!");
         System.out.println("Nueva Rama Myriam");
-        System.out.println("Nuevo commit Myriam");
     }
 }
