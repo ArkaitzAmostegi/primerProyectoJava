21689c6b505a5c1fb4c7226c74ccc6d349b1dc51
Myriam <ikdgk@plaiaundi.net>
2024-12-11 10:57:53 +0100

    Commit suma

--- src/App.java
+++ src/App.java
@@ -7,6 +7,9 @@ public class App {
         }
         System.out.println("Kaixo, nuevo commit M");
         System.out.println("Conflito!");
-
+        int variable= 5;
+        int variable2= 2;
+        int resultado= variable+variable2;
+        System.out.println(resultado);
     }
 }
